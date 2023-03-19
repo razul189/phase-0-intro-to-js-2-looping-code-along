bf241def5fbb0be27cc47fe4a3760c93bc26df12
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-03-19 00:00:08 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"]
+// for (let i = 0; i < gifts.length; i++) {
+//     console.log (`wrapped ${gifts[i]} and added a bow!`)
+//     debugger
+// }
+// return gifts 
+// wrappedGifts(gifts)
+
+const names = ['Guadalupe', 'Ollie', 'Aki']
+function writeCards (names){
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        let  message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        messages.push(message)
+    }
+    return messages
+}
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num)
+        num-- 
+    }
+} 
+
